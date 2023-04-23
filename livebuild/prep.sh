175fddb671b49f79c87fb05cb38df20b985e77fb
rkarsnk <git-repos-rkarsnk@rkarsnk.jp>
2023-04-23 15:27:52 +0900

    1st commit.

---
+++ livebuild/prep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TIMESTAMP=`date +%Y%m%d`
+IMAGE_NAME="PLDE_alpha1_${TIMESTAMP}"
+
+
+lb config \
+	--apt apt \
+	--apt-recommends false \
+	--architecture amd64 \
+	--distribution testing  \
+	--parent-distribution testing \
+	--parent-debian-installer-distribution testing \
+	--archive-areas "main contrib non-free non-free-firmware" \
+	--mirror-bootstrap "http://ftp.riken.go.jp/Linux/debian/debian" \
+	--mirror-chroot "http://ftp.riken.go.jp/Linux/debian/debian" \
+	--mirror-binary "http://ftp.riken.go.jp/Linux/debian/debian" \
+	--bootappend-live "boot=live components locales=ja_JP.UTF-8 debug=1" \
+	--image-name ${IMAGE_NAME}
+
