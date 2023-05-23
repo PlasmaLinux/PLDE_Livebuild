0dfc96a7d73e57d3c4898efe021c1019348f9030
aruelu <arugacchi@gmail.com>
2023-05-23 10:42:37 +0900

    sedの記載修正

--- replace_version_description.sh
+++ replace_version_description.sh
@@ -6,35 +6,24 @@
 CHROOT_AFTER_DIR="config/includes.chroot_after_packages"
 BOOTLOADERS_DIR="config/bootloaders"
 
-sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
-                           ${DISTRO_VERSION}\
-                           ${DISTRO_CODENAME}#g" \
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME} ${DISTRO_VERSION} ${DISTRO_CODENAME}#g" \
     -e "s#@NAME@#${DISTRO_NAME}#g" \
     -e "s#@VERSION_ID@#${DISTRO_VERSION}#g" \
-    -e "s#@VERSION_NAME@#${DISTRO_VERSION}\
-                         ${DISTRO_CODENAME}#g" \
+    -e "s#@VERSION_NAME@#${DISTRO_VERSION} ${DISTRO_CODENAME}#g" \
     ${CHROOT_AFTER_DIR}/lib/os-release
 
-sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
-                           ${DISTRO_VERSION}\
-                           ${DISTRO_CODENAME}#g" \
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME} ${DISTRO_VERSION} ${DISTRO_CODENAME}#g" \
     ${CHROOT_AFTER_DIR}/etc/issue
 
-sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
-                           ${DISTRO_VERSION}\
-                           ${DISTRO_CODENAME}#g" \
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME} ${DISTRO_VERSION} ${DISTRO_CODENAME}#g" \
     ${CHROOT_AFTER_DIR}/etc/issue.net
 
 sed -i -e "s#NAME@#${DISTRO_NAME}#g" \
     ${CHROOT_AFTER_DIR}/etc/default/grub
 
-sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
-                           ${DISTRO_VERSION}\
-                           ${DISTRO_CODENAME}#g" \
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME} ${DISTRO_VERSION} ${DISTRO_CODENAME}#g" \
     ${BOOTLOADERS_DIR}/syslinux_common/splash.svg
 
-sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
-                           ${DISTRO_VERSION}\
-                           ${DISTRO_CODENAME}#g" \
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME} ${DISTRO_VERSION} ${DISTRO_CODENAME}#g" \
     ${BOOTLOADERS_DIR}/grub-pc/live-theme/theme.txt
 
