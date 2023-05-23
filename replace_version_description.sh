754150f1e4cab245cadaf5bbf0488276106bf89d
aruelu <arugacchi@gmail.com>
2023-05-23 10:02:01 +0900

    VERSION内の変数の見直し

--- replace_version_description.sh
+++ replace_version_description.sh
@@ -6,18 +6,35 @@
 CHROOT_AFTER_DIR="config/includes.chroot_after_packages"
 BOOTLOADERS_DIR="config/bootloaders"
 
-sed -i -e "s#@PRETTY_NAME@#${PRETTY_NAME}#g" \
-    -e "s#@NAME@#${NAME}#g" \
-    -e "s#@VERSION_ID@#${VERSION_ID}#g" \
-    -e "s#@VERSION_NAME@#${VERSION_NAME}#g" \
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
+                           ${DISTRO_VERSION}\
+                           ${DISTRO_CODENAME}#g" \
+    -e "s#@NAME@#${DISTRO_NAME}#g" \
+    -e "s#@VERSION_ID@#${DISTRO_VERSION}#g" \
+    -e "s#@VERSION_NAME@#${DISTRO_VERSION}\
+                         ${DISTRO_CODENAME}#g" \
     ${CHROOT_AFTER_DIR}/lib/os-release
 
-sed -i -e "s#@PRETTY_NAME@#${PRETTY_NAME}#g" ${CHROOT_AFTER_DIR}/etc/issue
-sed -i -e "s#@PRETTY_NAME@#${PRETTY_NAME}#g" ${CHROOT_AFTER_DIR}/etc/issue.net
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
+                           ${DISTRO_VERSION}\
+                           ${DISTRO_CODENAME}#g" \
+    ${CHROOT_AFTER_DIR}/etc/issue
 
-sed -i -e "s#NAME@#${NAME}#g" ${CHROOT_AFTER_DIR}/etc/default/grub
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
+                           ${DISTRO_VERSION}\
+                           ${DISTRO_CODENAME}#g" \
+    ${CHROOT_AFTER_DIR}/etc/issue.net
 
-sed -i -e "s#@PRETTY_NAME@#${PRETTY_NAME}#g" ${BOOTLOADERS_DIR}/syslinux_common/splash.svg
+sed -i -e "s#NAME@#${DISTRO_NAME}#g" \
+    ${CHROOT_AFTER_DIR}/etc/default/grub
 
-sed -i -e "s#@PRETTY_NAME@#${PRETTY_NAME}#g" ${BOOTLOADERS_DIR}/grub-pc/live-theme/theme.txt
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
+                           ${DISTRO_VERSION}\
+                           ${DISTRO_CODENAME}#g" \
+    ${BOOTLOADERS_DIR}/syslinux_common/splash.svg
+
+sed -i -e "s#@PRETTY_NAME@#${DISTRO_NAME}\
+                           ${DISTRO_VERSION}\
+                           ${DISTRO_CODENAME}#g" \
+    ${BOOTLOADERS_DIR}/grub-pc/live-theme/theme.txt
 
